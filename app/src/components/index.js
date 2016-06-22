f4f0113e56afa9c8cf9e381b8108c89fe75de8ff
Yvonne Hayes <queyvonne@gmail.com>
2016-06-22 14:27:51 -0700

    added thumbnail

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,11 +1,13 @@
 import angular from 'angular';
 import app from './app/app';
 import text from './text/text';
+import thumb from './thumb/thumb';
 
 
 const components = angular.module('components', [])
   .component('app',app)
-  .component('text',text);
+  .component('text',text)
+  .component('thumb', thumb);
 
 
 
---
+++ app/src/components/thumb/thumb.js
@@ -0,0 +1,13 @@
+import template from './thumb.html';
+
+export default {
+  template,
+  controllerAs: 'thumb',
+  require: {
+    app:'^^'
+  },
+  controller() {
+
+
+  }
+};
