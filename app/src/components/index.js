53bed91ab02e64f0b64a41dbef5f85cac1e4ef59
David <goodwid@gmail.com>
2016-06-23 13:13:03 -0700

    Fixed regex in index.js

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -5,15 +5,13 @@ import path from 'path';
 const reqContext = require.context(
     './',
     true,
-    /.js$/
+    /^\.\/(?!index).+?\.js$/
 );
 
 const components = angular.module( 'components', [] );
 
 reqContext.keys().forEach( key => {
-  if (key.startsWith('./index')) return;
   const name = camelcase( path.basename( key, '.js' ) );
-
   components.component( name, reqContext( key ).default );
 });
 
