05e558c5b441ee95d024572a3a87c6bee39b6eb1
David <goodwid@gmail.com>
2016-06-27 14:49:35 -0700

    Moved babel defaults to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -31,10 +31,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015']
-        }
+        loader: 'babel'
       }
     ]
   },
