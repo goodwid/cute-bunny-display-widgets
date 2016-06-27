6f8e667bb52677830c4a5e10c64a6e1257d9276a
David <goodwid@gmail.com>
2016-06-27 13:25:54 -0700

    Full test working

--- app/test/full.test.js
+++ app/test/full.test.js
@@ -2,21 +2,30 @@
 
 const assert = chai.assert;
 
-describe('directive', () => {
+describe('slideshow', () => {
   angular.mock.module.sharedInjector();
   before(angular.mock.module('components'));
 
-
   let $componentController;
   before(angular.mock.inject(($rootScope, _$componentController_) => {
-    $componentController = _componentController_;
+    $componentController = _$componentController_;
   }));
 
-  it ('next function brings on next bunny picture', () => {
-    let result;
-    const next = () => {
-      result =
-    };
+  it ('next function increases index', () => {
+    const full = $componentController('full', null, {data});
+    const data = [0,1,2,3];
+    full.data = data;
+    full.next();
+    assert.equal(full.index, 1);
 
+    full.prev();
+    assert.equal(full.index, 0);
+
+    full.prev();
+    assert.equal(full.index, 3);
+
+    full.next();
+    assert.equal(full.index, 0);
   });
+
 });
