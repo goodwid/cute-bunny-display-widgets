c27f3e67824076386da7b5d2453aebfa985c98e1
Yvonne Hayes <queyvonne@gmail.com>
2016-06-28 14:36:26 -0700

    added services folder, index.js and wrapped api in services

--- src/bunny-app.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular';
-import components from './components';
-
-const app = angular.module('bunnyApp', [components]);
-
-export default app.name;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import angular from 'angular';
-import app from './bunny-app';
+import app from './photoAlbum';
 import './scss/main.scss';
+
+app.value( 'apiUrl', 'http://localhost:9000/api');
 
 angular.bootstrap(document, [app]);
