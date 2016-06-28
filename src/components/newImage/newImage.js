4209e848078fad3e476dcd27f1a0ca7418e2c6db
David <goodwid@gmail.com>
2016-06-28 16:06:57 -0700

    Added album dropdown to new image

--- src/components/newImage/newImage.js
+++ src/components/newImage/newImage.js
@@ -7,13 +7,20 @@ export default {
   bindings: {
     add: '&'
   },
-  controller() {
-    this.styles = styles;
-    this.submit = () => {
-      let Image = this.Image;
-      this.add({Image});
-      this.Image = {};
-
-    };
-  }
+  controller
 };
+
+controller.inject = ['albumService'];
+
+function controller (albumService) {
+  albumService.get()
+    .then(albums => {
+      this.albums = albums;
+    });
+  this.styles = styles;
+  this.submit = () => {
+    let image = this.image;
+    this.add({image});
+    this.image = {};
+  };
+}
