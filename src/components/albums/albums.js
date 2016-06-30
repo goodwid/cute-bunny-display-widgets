a0eda7a6ec1b14b7dd528f3a6ed6ceef3d12403f
David <goodwid@gmail.com>
2016-06-30 14:16:49 -0700

    Still semi-working route links??

--- src/components/albums/albums.js
+++ src/components/albums/albums.js
@@ -8,9 +8,46 @@ export default {
   controller
 };
 
-controller.inject = ['albumService'];
+controller.inject = ['albumService', 'imageService'];
 
-function controller(albumService) {
+function controller(albumService, imageService) {
   albumService.get()
     .then(albums => this.albums = albums);
+
+  this.getImages = (album) => {
+    imageService.getImagesByAlbum(album)
+      .then(images => this.images = images)
+      .catch(err => console.error(err));
+  };
+  this.addImage = (image) => {
+    imageService.add(image)
+      .then(image => this.images.push(image))
+      .catch(err => console.error(err));
+  };
+  this.addAlbum = (album) => {
+    albumService.add(album)
+      .then(album => this.albums.push(album))
+      .catch(err => console.error(err));;
+  };
+  this.deleteImage = (id) => {
+    imageService.delete(id)
+      .then(() => {
+        const index = this.images.findIndex(image => image._id === id);
+        if (index !== -1) this.images.splice(index,1);
+      })
+      .catch(err => console.error(err));
+    imageService.getImagesByAlbum(this.albumId).then(images => this.images = images);
+  };
+  imageService.get().then(images => this.images = images);
+  albumService.get().then(albums => this.albums = albums);
+
+
+  // default view
+  this.view = 'tile';
+
+
+
+
+
+
 }
--- src/routes.js
+++ src/routes.js
@@ -4,19 +4,28 @@ export default function configRoutes($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: '/',
-      template: '<p>Home again home again</p>'
+      template: '<p><a ui-sref="albums">Albums</a>Home again home again</p>'
     })
+    // .state('albumlist', {
+    //   url: '/albumlist'
+    // })
     .state('albums', {
       url: '/albums',
 
-
+      component: 'albums'
     })
-    .state('album', {
-      url: '/album/:albumId?display',
+    .state('albums.tile', {
+      url: '/:albumId',
+      component: 'tile',
       resolve: {
-        albumName: ['albumService', '$stateParams', (aS, sP) => aS.get(sP.albumId)],
-        display: ['$stateParams', sP => sP.display]
-      }
+        albumName: ['albumService', '$stateParams', (aS, sP) => {
+          aS.get(sP.albumId);
+        }],
+        display: ['$stateParams', sP => {
+          console.log(sP.display);
+          console.log(sP);
+        }]
+      },
     });
   $urlRouterProvider.otherwise('/');
 }
