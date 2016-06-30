7feb3c14dc17b3f28dc851c5e11cd95ae1c3cc83
David <goodwid@gmail.com>
2016-06-30 09:22:19 -0700

    Add and remove now dynamically update the images on the page

--- src/components/viewSelector/viewSelector.js
+++ src/components/viewSelector/viewSelector.js
@@ -12,18 +12,27 @@ controller.inject = ['albumService', 'imageService'];
 function controller(albumService, imageService) {
   this.styles = styles;
   this.getImages = (album) => {
-    imageService.getImagesByAlbum(album).then(images => this.images = images);
+    imageService.getImagesByAlbum(album)
+      .then(images => this.images = images)
+      .catch(err => console.error(err));
   };
   this.addImage = (image) => {
-    imageService.add(image);
-    imageService.getImagesByAlbum(this.albumId).then(images => this.images = images);
+    imageService.add(image)
+      .then(image => this.images.push(image))
+      .catch(err => console.error(err));
   };
   this.addAlbum = (album) => {
-    albumService.add(album);
-    albumService.get().then(albums => this.albums = albums);
+    albumService.add(album)
+      .then(album => this.albums.push(album))
+      .catch(err => console.error(err));;
   };
   this.deleteImage = (id) => {
-    imageService.delete(id);
+    imageService.delete(id)
+      .then(() => {
+        const index = this.images.findIndex(image => image._id === id);
+        if (index !== -1) this.images.splice(index,1);
+      })
+      .catch(err => console.error(err));
     imageService.getImagesByAlbum(this.albumId).then(images => this.images = images);
   };
   imageService.get().then(images => this.images = images);
